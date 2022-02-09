28616b6b011a1f62c17a8d54a228f17be6a8b0f2
Emre Ç <superbevent@hotmail.com>
2022-02-09 10:58:29 +0300

    Add project files.

---
+++ CDASLiteEntityLayer/IdentityModels/AppRole.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDASLiteEntityLayer.IdentityModels
+{
+    public class AppRole : IdentityRole
+    {
+        [StringLength(400, ErrorMessage = "Role Description can have utmost 400 characters!")]
+        public string Description{ get; set; }
+    }
+}
---
+++ CDASLiteEntityLayer/IdentityModels/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using CDASLiteEntityLayer.Enums;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDASLiteEntityLayer.IdentityModels
+{
+    public class AppUser : IdentityUser
+    {
+        public string Picture { get; set; }
+
+        public DateTime? BirtDate { get; set; }
+
+        public Genders Gender { get; set; }
+    }
+}
