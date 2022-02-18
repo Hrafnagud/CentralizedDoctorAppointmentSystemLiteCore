c95176995937a8d419ff331e0d77fc7f87650ee0
Emre Ç <superbevent@hotmail.com>
2022-02-18 11:11:51 +0300

    City District insert queries added Index and appointment actions for Patient controller implemented.

---
+++ CDASLiteUI/Controllers/PatientController.cs
@@ -0,0 +1,61 @@
+﻿using CDASLiteBusinessLogicLayer.Contracts;
+using CDASLiteBusinessLogicLayer.EmailService;
+using CDASLiteEntityLayer.IdentityModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CDASLiteUI.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly RoleManager<AppRole> roleManager;
+        private readonly IEmailSender emailSender;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IConfiguration configuration;
+
+        public PatientController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager, IEmailSender emailSender, IUnitOfWork unitOfWork, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+            this.emailSender = emailSender;
+            this.unitOfWork = unitOfWork;
+            this.configuration = configuration;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        [Authorize]
+        public IActionResult Appointment()
+        {
+            try
+            {
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+    }
+}
