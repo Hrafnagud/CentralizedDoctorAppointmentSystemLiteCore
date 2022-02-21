3ab119c8e9e06865e0548d0ae0f82cc93e63ce7d
Emre Ç <superbevent@hotmail.com>
2022-02-21 14:54:14 +0300

    Appoitment for patient is being implemented...

---
+++ CDASLiteUI/Controllers/HospitalController.cs
@@ -0,0 +1,51 @@
+﻿using CDASLiteBusinessLogicLayer.Contracts;
+using CDASLiteBusinessLogicLayer.EmailService;
+using CDASLiteEntityLayer.IdentityModels;
+using CDASLiteEntityLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CDASLiteUI.Controllers
+{
+    public class HospitalController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly RoleManager<AppRole> roleManager;
+        private readonly IEmailSender emailSender;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IConfiguration configuration;
+
+        public HospitalController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager, IEmailSender emailSender, IUnitOfWork unitOfWork, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+            this.emailSender = emailSender;
+            this.unitOfWork = unitOfWork;
+            this.configuration = configuration;
+        }
+
+        public JsonResult GetHospitalFromClinicId(int id, int districtid)
+        {
+            try
+            {
+                var data = new List<Hospital>();
+                if (id > 0 && districtid > 0)
+                {
+                    data = unitOfWork.HospitalClinicRepository.GetAll(x => x.ClinicId == id).Select(y => y.Hospital).Where(x => x.DistrictId == districtid).Distinct().ToList();
+                }
+                return Json(new { isSuccess = true, data });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { isSuccess = false });
+            }
+        }
+    }
+}
