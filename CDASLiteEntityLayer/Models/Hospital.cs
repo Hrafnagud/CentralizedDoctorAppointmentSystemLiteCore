857dcdd254f149539ac65290c22646cf4f27d00d
Emre Ç <superbevent@hotmail.com>
2022-02-21 11:47:41 +0300

    Hospital table has been rearranged and related migration added.

--- CDASLiteEntityLayer/Models/Hospital.cs
+++ CDASLiteEntityLayer/Models/Hospital.cs
@@ -16,8 +16,21 @@ namespace CDASLiteEntityLayer.Models
         public string HospitalName { get; set; }
 
         public int DistrictId { get; set; } //Every Hospital will have a District, we should add Hospital List in District class!
-        [ForeignKey("DistrictId")]
 
+        [StringLength(500, ErrorMessage = "Adress details must contain utmost 500 characters!")]
+        public string Address { get; set; }
+
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone number must contain exactly 10 characters, without zero at the beginning!")]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+
+        [ForeignKey("DistrictId")]
         public virtual District HospitalDistrict { get; set; }
 
         public virtual List<HospitalClinic> HospitalClinics { get; set; }  //Hospital - HospitalClinics relationship
