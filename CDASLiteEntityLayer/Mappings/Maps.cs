5d902714f74a65f44f2beac1eba105670034481e
Emre Ç <superbevent@hotmail.com>
2022-02-28 10:01:09 +0300

    Auto Mapper added. Paginated List also added for pagination.

---
+++ CDASLiteEntityLayer/Mappings/Maps.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CDASLiteEntityLayer.Models;
+using CDASLiteEntityLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDASLiteEntityLayer.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            //CreateMap<Appointment, AppointmentVM>();
+            //CreateMap<AppointmentVM, Appointment>();
+            //To simplify above statements, we can use reversemap method so that they can be interchangeably used
+            CreateMap<AppointmentVM, Appointment>().ReverseMap();
+        }
+    }
+}
--- CDASLiteEntityLayer/Models/Base.cs
+++ CDASLiteEntityLayer/Models/Base.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CDASLiteEntityLayer.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -10,13 +11,18 @@ namespace CDASLiteEntityLayer.Models
 {
     public class Base<T> : IBase
     {
-        [Key]
-        [Column(Order = 1)]
         public T Id { get; set; }
-
-        [Column(Order = 2)]
-        [DataType(DataType.DateTime)]
-        [Display(Name = "Creation Date")]
         public DateTime CreatedDate{ get; set; }
+        [Required]
+        public string PatientId { get; set; }
+        public int HospitalClinicId { get; set; }
+        [Required]  
+        public DateTime AppointmentDate { get; set; }
+        [Required]
+        public string AppointmentHour { get; set; }
+        public AppointmentStatus AppointmentStatus { get; set; }
+        public Patient Patient { get; set; }
+        public HospitalClinic HospitalClinic { get; set; }
+
     }
 }
