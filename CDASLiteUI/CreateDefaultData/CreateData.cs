1d9c8332668e567376db448963738ba201e31764
Emre Ç <superbevent@hotmail.com>
2022-03-07 11:19:38 +0300

    Import excels are proceeding.

--- CDASLiteUI/CreateDefaultData/CreateData.cs
+++ CDASLiteUI/CreateDefaultData/CreateData.cs
@@ -21,7 +21,60 @@ namespace CDASLiteUI.CreateDefaultData
             //Method calls to add data.
             //CheckRoles(roleManager);
             CreateCities(environment, unitOfWork);
+            //This method is suitable to implement CheckRoles or Cities only. Other tables such as hospitals, clinics (has loads of data) would be not so cost efficient.
             CreateClinics(environment, unitOfWork);
+            //These guys will be added via scripts. Too Expensive for server side.
+            #if DEBUG
+                CreateDistricts(environment, unitOfWork);
+                //CreateDistricts(environment, unitOfWork);
+            #endif
+        }
+
+        private static void CreateDistricts(IWebHostEnvironment environment, IUnitOfWork unitOfWork)
+        {
+            try
+            {
+                var districtList = unitOfWork.DistrictRepository
+                    .GetAll().ToList();
+
+                //Provide a path for excel file
+                // Excel dosyasının bulunduğu yolu aldık
+                string path = Path.Combine(environment.WebRootPath, "Excels");
+                string fileName = Path.GetFileName("Districts.xlsx");
+                string filePath = Path.Combine(path, fileName);
+                using (var excelBook = new XLWorkbook(filePath))
+                {
+                    var rows = excelBook.Worksheet(1).RowsUsed();
+                    foreach (var item in rows)
+                    {
+                        if (item.RowNumber() > 1
+                            && item.RowNumber() <= rows.Count())
+                        {
+                            var cell = item.Cell(1).Value; //ilçe adı
+                            var cityId = Convert.ToByte(item.Cell(2).Value);//1
+                            var city = unitOfWork.CityRepository.GetFirstOrDefault(x => x.Id == cityId);
+                            District district = new District()
+                            {
+                                DistrictName = cell.ToString(),
+                                CityId = cityId,
+                                CreatedDate = DateTime.Now
+                            };
+
+                            if (districtList.Count(x => x.DistrictName.ToLower() == cell.ToString().ToLower() && x.CityId == cityId) == 0)
+                            {
+                                unitOfWork.DistrictRepository.Add(district);
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
         }
 
         private static void CheckRoles(RoleManager<AppRole> roleManager)
